9bf7a17c3f15e7a3ecfebe10cb33a874a771baf0
reshi-sahiba <167600555+reshi-sahiba@users.noreply.github.com>
2024-04-28 20:18:30 +0530

    Update srollscript.js
    change in js

--- website/js/srollscript.js
+++ website/js/srollscript.js
@@ -1,1 +1,23 @@
 
+	const scrollTopBtn=document.getElementById("scrollTopBtn");
+        windows.onscroll = function()
+	{scrollFunction()};
+
+	function scrollFunction()
+	{
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+		{
+			scrollTopBtn.style.display = "block";
+		}
+	else
+		{
+			scrollTopBtn.style.display = "none";
+		}
+	}
+
+	function topFunction()
+	{
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	};
+
